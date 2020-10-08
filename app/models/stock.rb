884b0b1196090d380807be8f912df74d95082f8a
Kazi Khaled Saif <kksaif05@gmail.com>
2020-10-08 15:16:35 +0600

    Stock Search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,16 @@
 class Stock < ApplicationRecord
+
+  def self.new_from_lookup(tinker_symbol)
+    begin
+      StockQuote::Stock.new(api_key: 'sk_01939fc7ea04450691d3edd89beb7251')
+      looked_up_stock =  StockQuote::Stock.quote(tinker_symbol)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue
+      return nil
+    end
+  end
+
+
+
+
 end
--- db/migrate/20201007115837_create_stocks.rb
+++ db/migrate/20201007115837_create_stocks.rb
@@ -1,7 +1,7 @@
 class CreateStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :stocks do |t|
-      t.strng :ticker
+      t.string :ticker
       t.string :name
       t.decimal :last_price
 
